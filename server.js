0b3472cd91516ad1c8063cd2dab6becf905f579b
yero0214 <yero0214@gmail.com>
2021-04-23 14:02:34 +0900

    push

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const { response } = require('express');
 const riotKey = "RGAPI-877660af-9286-438c-996d-2d3ae95c1d02";
 
 const PORT = 3000;
@@ -16,39 +17,52 @@ app.use(express.urlencoded({
     extended: true
 }))
 
-app.get("/eid", async (req, res) => {
-    const name = req.query.name;
-  
-    await axios.get("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + encodeURI(name), {
-      headers: {"X-Riot-Token": riotKey},
-    })
-        .then((response) => {
-          console.log(response.data.id);
-          axios.get("https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/" + encodeURI(response.data.id), {
-            headers: {"X-Riot-Token": riotKey},
-          })
-              .then((response) => {
-                console.log(response.data);
-                res.send(response.data);
-              })
-              .catch((error) => {
-                console.log(error.message);
-                res.send(error.message);
-              });
-        })
-        .catch((error) => {
-          console.log(error.message);
-          res.send(error.message);
-        });
+app.get("/", async (req, res) => {
+    let result = [];
+
+    try{
+      const eid = await userInfo(req.query.name);
+      result.push(eid);
+    
+    } catch(e){
+      console.log('failed!');
+      console.error(e);
+      res.send(e.message);
+    }
+
+    const waitingTier = getTier(result[0].id);
+
+
+    
+    try{
+      const tier = await waitingTier;
+      
+      if(tier.length == 0){
+        result.push("unranked")
+      } else{
+        result.push(tier);
+      }
+      
+    } catch(e){
+      console.log('failed!');
+      console.error(e);
+      res.send(e.message);
+    }
+
+    res.send(result);
+
+
+    async function userInfo(name){
+      let id = await axios.get("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + encodeURI(name), {
+        headers: {"X-Riot-Token": riotKey},
+      })
+      return id.data;
+    };
+    async function getTier(id){
+      let tier = await axios.get("https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/" + encodeURI(id), {
+        headers: {"X-Riot-Token": riotKey},
+      })
+      return tier.data;
+    };
   });
-// async function getInfo(eid){
-//     const info = '';
-//     await axios.get('https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/' + encodeURI(eid), {
-//             headers:{'X-Riot-Token': 'RGAPI-27005fbf-01a5-45ea-bb00-218f51912675'}
-//         })
-//         .then(response => (info = response.data))
-//         .catch(error => (console.log(error.message)))
-//         return info;
-        
-// }
 app.listen(PORT, HOST);
